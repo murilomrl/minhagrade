14c28ba500d9d93a02ec089ac69685addfb64afa
Murilo Moura <murilomrlsouza@gmail.com>
2017-03-27 17:07:17 -0300

    Etapa 1 concluida!

--- app/src/main/java/com/devmob/minhagrade/CourseActivity.java
+++ app/src/main/java/com/devmob/minhagrade/CourseActivity.java
@@ -1,15 +1,21 @@
 package com.devmob.minhagrade;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static android.R.attr.data;
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class CourseActivity extends AppCompatActivity {
 
@@ -35,10 +41,32 @@ public class CourseActivity extends AppCompatActivity {
         list.add("Fisica");
         list.add("Matematica");
         ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_spinner_item, list);
+                android.R.layout.simple_spinner_item, list){
+
+            public View getView(int position, View convertView,ViewGroup parent) {
+
+                View v = super.getView(position, convertView, parent);
+
+                ((TextView) v).setTextSize(26);
+
+                return v;
+
+            }
+
+            public View getDropDownView(int position, View convertView,ViewGroup parent) {
+
+                View v = super.getDropDownView(position, convertView,parent);
+
+                ((TextView) v).setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+                ((TextView) v).setTextSize(26);
+                return v;
+
+            }
+        };
         dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner2.setAdapter(dataAdapter);
     }
+
 
     public void addListenerOnSpinnerItemSelection() {
         spinner1 = (Spinner) findViewById(R.id.spinner);
@@ -55,11 +83,14 @@ public class CourseActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-
-                Toast.makeText(CourseActivity.this,
+                Intent intent = new Intent(CourseActivity.this, ProgressActivity.class);
+                String message = String.valueOf(spinner1.getSelectedItem());
+                intent.putExtra("MESSAGE", message);
+                CourseActivity.this.startActivity(intent);
+                /*Toast.makeText(CourseActivity.this,
                         "OnClickListener : " +
                                 "\nSpinner : "+ String.valueOf(spinner1.getSelectedItem()),
-                        Toast.LENGTH_SHORT).show();
+                        Toast.LENGTH_SHORT).show();*/
             }
 
         });
---
+++ app/src/main/java/com/devmob/minhagrade/ProgressActivity.java
@@ -0,0 +1,53 @@
+package com.devmob.minhagrade;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+/**
+ * Created by devmob on 27/03/17.
+ */
+
+public class ProgressActivity extends Activity{
+    TextView text;
+
+    private static final int PROGRESS = 0x1;
+
+    private ProgressBar mProgress;
+    private int mProgressStatus = 0;
+
+    private Handler mHandler = new Handler();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_progress);
+        super.onCreate(savedInstanceState);
+        Intent intent = getIntent();
+        String value =  intent.getStringExtra("MESSAGE");
+        //text = (TextView)findViewById(R.id.textView);
+        //text.setText(value);
+
+
+
+
+        mProgress = (ProgressBar) findViewById(R.id.progressBar);
+
+        //Start lengthy operation in a background thread
+        new Thread(new Runnable() {
+            public void run() {
+                while (mProgressStatus < 100) {
+                    mProgressStatus = 1;
+
+                    // Update the progress bar
+                    mHandler.post(new Runnable() {
+                        public void run() {
+                            mProgress.setProgress(mProgressStatus);
+                        }
+                    });
+                }
+            }
+        }).start();
+    }
+}

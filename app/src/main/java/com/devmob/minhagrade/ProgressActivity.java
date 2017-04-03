7890f8fdf2cc6f2e529ed6724acf98d08f696ab7
Murilo Moura <murilomrlsouza@gmail.com>
2017-04-03 14:19:08 -0300

    Coloquei um botão pro primeiro periodo, mas acho que seja legal usar uma dynamicView para gerar os outros botões sozinho

--- app/src/main/java/com/devmob/minhagrade/ProgressActivity.java
+++ app/src/main/java/com/devmob/minhagrade/ProgressActivity.java
@@ -4,8 +4,10 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by devmob on 27/03/17.
@@ -13,6 +15,8 @@ import android.widget.TextView;
 
 public class ProgressActivity extends Activity{
     TextView text;
+    private Button button;
+
 
     private static final int PROGRESS = 0x1;
 
@@ -26,28 +30,40 @@ public class ProgressActivity extends Activity{
         super.onCreate(savedInstanceState);
         Intent intent = getIntent();
         String value =  intent.getStringExtra("MESSAGE");
+        
+
+
         //text = (TextView)findViewById(R.id.textView);
         //text.setText(value);
 
 
 
 
-        mProgress = (ProgressBar) findViewById(R.id.progressBar);
+//        mProgress = (ProgressBar) findViewById(R.id.progressBar);
 
         //Start lengthy operation in a background thread
-        new Thread(new Runnable() {
-            public void run() {
-                while (mProgressStatus < 100) {
-                    mProgressStatus = 1;
+//        new Thread(new Runnable() {
+//            public void run() {
+//                while (mProgressStatus < 100) {
+//                    mProgressStatus = 1;
+//
+//                    // Update the progress bar
+//                    mHandler.post(new Runnable() {
+//                        public void run() {
+//                            mProgress.setProgress(mProgressStatus);
+//                        }
+//                    });
+//                }
+//            }
+//        }).start();
+    }
+    public void addListenerButton(){
+        button = (Button)findViewById(R.id.periodo);
+        String msg =  (String) button.getText();
+        Toast.makeText(this,
+                "OnClickListener : " +
+                        "\n"+ msg,
+                Toast.LENGTH_SHORT).show();
 
-                    // Update the progress bar
-                    mHandler.post(new Runnable() {
-                        public void run() {
-                            mProgress.setProgress(mProgressStatus);
-                        }
-                    });
-                }
-            }
-        }).start();
     }
 }

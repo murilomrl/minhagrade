814dad754b6434e94998fd8015ed782a7fdf2cb8
luizfdsr <luizfdsr@gmail.com>
2017-04-05 14:35:11 -0300

    Dynamic view

--- app/src/main/java/com/devmob/minhagrade/ProgressActivity.java
+++ app/src/main/java/com/devmob/minhagrade/ProgressActivity.java
@@ -4,7 +4,11 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -13,7 +17,7 @@ import android.widget.Toast;
  * Created by devmob on 27/03/17.
  */
 
-public class ProgressActivity extends Activity{
+public class ProgressActivity extends AppCompatActivity {
     TextView text;
     private Button button;
 
@@ -28,9 +32,41 @@ public class ProgressActivity extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
         setContentView(R.layout.activity_progress);
         super.onCreate(savedInstanceState);
-        Intent intent = getIntent();
-        String value =  intent.getStringExtra("MESSAGE");
-        
+//        Intent intent = getIntent();
+//        String value =  intent.getStringExtra("MESSAGE");
+
+//        Referencia de criação de elementos dinamicos no layout
+//        Site: http://androidexample.com/Dynamically_Create_View_Elements__-_Android_Example/index.php?view=article_discription&aid=115
+
+        final LinearLayout lm = (LinearLayout) findViewById(R.id.activity_progressbar);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+        for (int i = 1; i<=9; i++){
+            final int index = i;
+            LinearLayout ll = new LinearLayout(this);
+            ll.setOrientation(LinearLayout.HORIZONTAL);
+
+            final Button btn = new Button(this);
+            btn.setId(index);
+            btn.setText(index+"º Periodo");
+
+            btn.setLayoutParams(params);
+
+            btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Log.i("TAG", "index :" + index);
+
+                    Toast.makeText(getApplicationContext(),"Clicked Button Index :" + index, Toast.LENGTH_LONG).show();
+                }
+            });
+
+            ll.addView(btn);
+
+            lm.addView(ll);
+        }
+
+
 
 
         //text = (TextView)findViewById(R.id.textView);
@@ -57,13 +93,13 @@ public class ProgressActivity extends Activity{
 //            }
 //        }).start();
     }
-    public void addListenerButton(){
-        button = (Button)findViewById(R.id.periodo);
-        String msg =  (String) button.getText();
-        Toast.makeText(this,
-                "OnClickListener : " +
-                        "\n"+ msg,
-                Toast.LENGTH_SHORT).show();
-
-    }
+//    public void addListenerButton(){
+//        button = (Button)findViewById(R.id.periodo);
+//        String msg =  (String) button.getText();
+//        Toast.makeText(this,
+//                "OnClickListener : " +
+//                        "\n"+ msg,
+//                Toast.LENGTH_SHORT).show();
+//
+//    }
 }

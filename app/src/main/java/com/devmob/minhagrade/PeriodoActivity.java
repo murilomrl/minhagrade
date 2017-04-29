f509087bdc5f1abdea2e930d4a30eeecfa873efa
hylsonk <hylsonk@gmail.com>
2017-04-29 14:08:55 -0300

    Lista de periodos feito e com o evento onitemclick

---
+++ app/src/main/java/com/devmob/minhagrade/PeriodoActivity.java
@@ -0,0 +1,10 @@
+package com.devmob.minhagrade;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Nelson on 27/04/2017.
+ */
+
+class PeriodoActivity extends AppCompatActivity{
+}

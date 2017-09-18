56bf08f6c9b05f6242a1642cb45bd6fac87b693a
Hylson Kobayashi <hylsonk@gmail.com>
2017-09-18 18:59:46 -0300

    animação da splashscreen

--- app/src/main/java/com/devmob/minhagrade/SplashScreenActivity.java
+++ app/src/main/java/com/devmob/minhagrade/SplashScreenActivity.java
@@ -2,24 +2,47 @@ package com.devmob.minhagrade;
 
 import android.content.Intent;
 import android.os.Handler;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
-public class SplashScreenActivity extends AppCompatActivity implements  Runnable{
+import static java.lang.Thread.sleep;
+
+public class SplashScreenActivity extends AppCompatActivity implements Animation.AnimationListener {
+
+    Thread splashThread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-        Handler handler = new Handler();
-        handler.postDelayed(this,2000);
+
+        final ImageView imageView = (ImageView) findViewById(R.id.logoDevmob);
+        final Animation animation = AnimationUtils.loadAnimation(getBaseContext(),R.anim.alpha);
+        imageView.startAnimation(animation);
+        animation.setAnimationListener(this);
+    }
+
+    @Override
+    public void onAnimationStart(Animation animation) {
 
     }
 
     @Override
-    public void run() {
+    public void onAnimationEnd(Animation animation) {
         Intent intent = new Intent(SplashScreenActivity.this, CourseActivity.class);
-        startActivity(intent);
+        ActivityOptionsCompat opts = ActivityOptionsCompat.makeCustomAnimation(SplashScreenActivity.this,R.anim.alpha,R.anim.reverse_alpha);
+        ActivityCompat.startActivity(this,intent,opts.toBundle());
         finish();
+    }
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
+
     }
 }
